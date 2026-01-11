28f89f5cc1f97a87c2ca5ee6f095ea0bf71ead2c
tamstunman02 <tamstunman02@gmail.com>
2026-01-11 19:55:10 +0700

    Change CDN

---
+++ js/hammerjs-skypack.js
@@ -0,0 +1,16 @@
+/*
+ * Skypack CDN - hammerjs@2.0.8
+ *
+ * Learn more:
+ *   📙 Package Documentation: https://www.skypack.dev/view/hammerjs
+ *   📘 Skypack Documentation: https://www.skypack.dev/docs
+ *
+ * Pinned URL: (Optimized for Production)
+ *   ▶️ Normal: https://cdn.skypack.dev/pin/hammerjs@v2.0.8-UYK9WqzBrpqCSglWFVbD/mode=imports/optimized/hammerjs.js
+ *   ⏩ Minified: https://cdn.skypack.dev/pin/hammerjs@v2.0.8-UYK9WqzBrpqCSglWFVbD/mode=imports,min/optimized/hammerjs.js
+ *
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/hammerjs@v2.0.8-UYK9WqzBrpqCSglWFVbD/dist=es2019,mode=imports/optimized/hammerjs.js';
+export {default} from '/-/hammerjs@v2.0.8-UYK9WqzBrpqCSglWFVbD/dist=es2019,mode=imports/optimized/hammerjs.js';
